771f0033aa7b3e39a752926969fee0a445f9c29a
SudKul <sudhanshu.kulshrestha@gmail.com>
2022-06-15 17:09:20 +0530

    Upgrade Gradle and dependencies to AndroidX

--- app/src/main/java/com/example/android/flavor/MainActivity.java
+++ app/src/main/java/com/example/android/flavor/MainActivity.java
@@ -17,7 +17,7 @@ package com.example.android.flavor;
 
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.widget.ListView;
 
 import java.util.ArrayList;
